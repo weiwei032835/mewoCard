ceaae544fe60a8bb352481f04466d12161de5840
weiwei <wei>
2024-11-04 11:41:16 +0800

    修改按鈕置中

---
+++ js/資料.js
@@ -0,0 +1,60 @@
+
+//類別
+let tab_1 = [
+	{title:"品種", thumb:"tab_1_0.png"},
+	{title:"項鍊", thumb:"tab_1_1.png"},
+	{title:"裝飾", thumb:"tab_1_2.png"},
+	{title:"臉", thumb:"tab_1_3.png"},
+	{title:"背景", thumb:"tab_1_4.png"},
+	
+];
+
+
+//選項
+let options = [
+	//品種
+	{title:"白貓", thumb:"breed_0.png", pic:"breed_0_b.png"},
+	{title:"賓士貓", thumb:"breed_1.png", pic:"breed_1_b.png"},
+	{title:"小黃貓", thumb:"breed_2.png", pic:"breed_2_b.png"},
+	{title:"橘貓", thumb:"breed_3.png", pic:"breed_3_b.png"},
+	{title:"虎斑貓", thumb:"breed_4.png", pic:"breed_4_b.png"},
+	{title:"三花貓", thumb:"breed_5.png", pic:"breed_5_b.png"},
+	
+	//項圈
+	{title:"無", thumb:"necklace_del.png", pic:"necklace_del_b.png"},
+	{title:"鈴鐺", thumb:"necklace_0.png", pic:"necklace_0_b.png"},
+	{title:"球", thumb:"necklace_1.png", pic:"necklace_1_b.png"},
+	{title:"老鼠", thumb:"necklace_2.png", pic:"necklace_2_b.png"},
+	{title:"領巾", thumb:"necklace_3.png", pic:"necklace_3_b.png"},
+	{title:"蝴蝶結", thumb:"necklace_4.png", pic:"necklace_4_b.png"},
+
+	//配件
+	{title:"無", thumb:"deco_del", pic:"deco_del_b.png"},
+	{title:"橘子", thumb:"deco_1.png", pic:"breed_1_b.png"},
+	{title:"魚", thumb:"deco_2.png", pic:"deco_2_b.png"},
+	{title:"金元寶", thumb:"deco_3.png", pic:"deco_3_b.png"},
+	{title:"熊頭套", thumb:"deco_4.png", pic:"deco_4_b.png"},
+	{title:"毛帽", thumb:"deco_5.png", pic:"deco_5_b.png"},
+
+	//臉
+	{title:"無", thumb:"breed_5.png", pic:"breed_5_b.png"},
+	{title:"嘴巴", thumb:"breed_0.png", pic:"breed_0_b.png"},
+	{title:"圓框", thumb:"breed_1.png", pic:"breed_1_b.png"},
+	{title:"星星", thumb:"breed_2.png", pic:"breed_2_b.png"},
+	{title:"長方形", thumb:"breed_3.png", pic:"breed_3_b.png"},
+	{title:"桂冠", thumb:"breed_4.png", pic:"breed_4_b.png"},
+
+	//背景
+	{title:"腳印", thumb:"bg_0.png", pic:"bg_0_b.png"},
+	{title:"愛心", thumb:"bg_1.png", pic:"bg_1_b.png"},
+	{title:"紅色福", thumb:"bg_2.png", pic:"bg_2_b.png"},
+	{title:"散開", thumb:"bg_3.png", pic:"bg_3_b.png"},
+	{title:"日式藍", thumb:"bg_4.png", pic:"bg_4_b.png"},
+	{title:"草地", thumb:"bg_5.png", pic:"bg_5_b.png"},
+
+];
+
+
+
+
+
